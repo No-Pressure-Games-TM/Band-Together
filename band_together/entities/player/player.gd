677d8da29387ef3cde365e4d8746ce3bd970b77d
gareba <gaven@ualberta.ca>
2025-02-08 20:28:02 -0700

    feat: saxophone movement ability
    Dash is v btw :3
    upon pressing dash, the player charges a dash for 1 second, then dashes for 1 second, then returns to normal. Player also has fall speed reduced during the charge and dash
    
    Had to refactor a bit of older code to make more variables global, may cause a merge conflict in the future

--- band_together/entities/player/player.gd
+++ band_together/entities/player/player.gd
@@ -4,7 +4,19 @@ extends CharacterBody2D
 const SPEED: float = 100.0
 const JUMP_VELOCITY: float = -300.0
 var double_jump_count: int = 0
+
+var DASH_MULT = 1
+var GRAV_DIV = 1
 #region
+
+#region Dash bools
+var is_charging = false
+var is_dashing = false
+
+#region
+
+var direction = 0 
+
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -13,6 +25,9 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if velocity.y > 0 and (is_charging or is_dashing):
+			print("here?")
+			velocity.y /= GRAV_DIV
 	else:
 		double_jump_count = 0
 
@@ -25,13 +40,27 @@ func _physics_process(delta):
 			double_jump_count += 1
 	if Input.is_action_just_released("Accept") and velocity.y < -60:
 		velocity.y = - 60
+		
+	# Handle Dash - Will eventually be associated with the saxophones movement ability
+	if Input.is_action_just_pressed("Dash"):
+		print("DASH BUTTON PRESSED")
+		# The dash has 2 phases at the moment, the charge up, and then the execution. They should last for about 0.5
+		# seconds, then 1 second, respectively. 
+		if !is_charging and !is_dashing: # prevents repeated dashing
+			#Im also gonna implement a slow fall for the dash if youre in the air cause why not 
+			
+			GRAV_DIV = 2
+			
+			is_charging = true
+			$DashChargeTimer.start()
+			print("Charging")
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_axis("Left", "Right")
+	direction = Input.get_axis("Left", "Right")
 	if direction:
 		if sprite.animation != "walk":
 			sprite.play("walk")  # Play the running animation
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * DASH_MULT
 		# Flip the sprite based on direction
 		if direction > 0:
 			sprite.flip_h = false  # Face right
@@ -45,3 +74,32 @@ func _physics_process(delta):
 			sprite.play("idle")
 		
 	move_and_slide()
+	
+	
+
+
+func _on_dash_charge_timer_timeout() -> void:
+	
+	#start the dash
+	DASH_MULT = 2
+	is_charging = false
+	is_dashing = true
+	$DashExecuteTimer.start()
+	print("dashing")
+	
+	
+	
+	#start the timer for the dash
+
+
+func _on_dash_execute_timer_timeout() -> void:
+	
+	DASH_MULT = 1
+	GRAV_DIV = 1
+	is_dashing = false
+	print("dash over")
+	
+	
+	
+	
+	
