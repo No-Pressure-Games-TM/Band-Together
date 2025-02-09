ac702025a4a07d5eec0c483299dc7bbf8c23aa3d
gareba <gaven@ualberta.ca>
2025-02-09 01:39:29 -0700

    chore: cleaned up and documented player.gd

--- band_together/entities/player/player.gd
+++ band_together/entities/player/player.gd
@@ -6,28 +6,24 @@ const JUMP_VELOCITY: float = -300.0
 var double_jump_count: int = 0
 #region
 
-#keeps track of if the character has attached to the wall
+#region Walljump info
 var attached_to_wall = false
-var used_walljump = 0 # ternary value, it is 0 if the user has not recently used a walljump, -1 if they have
-#recently jumped off a left facing wall, or 1 if they have recently jumped off a right facing wall 
-
 var current_wall = 1
 var recent_wall = 0
-#could also instead keep a variable for the most recently touched wall, so that the player could jump off 
-#of two different walls that face the same direction that are just adjacent to each other. 
-#yea i think im gonna go with that one
+#region
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 ## Use double hashtags for a "title comment", it looks a lil different!
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity, whenever the player is both not on the ground or clinging to a wall
 	if not is_on_floor() and not attached_to_wall:
 		velocity += get_gravity() * delta
-	elif is_on_floor_only():
+	# Reset double and wall jumps when the player hits the ground
+	elif is_on_floor():
 		double_jump_count = 0
 		
-#double jumps currently reset when the player wall jumps, we can change that. 
+
 
 	#Handle down, so far only used if the player wants to detach from a wall
 	if Input.is_action_just_pressed("Down") and attached_to_wall:
@@ -40,36 +36,34 @@ func _physics_process(delta):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			recent_wall = null
-	
 		
+		#if clinging to a wall
 		elif attached_to_wall:
-			
 			velocity.y = JUMP_VELOCITY
 			attached_to_wall = false
 			recent_wall = current_wall
 			current_wall = null
 		
-			
+		#if going to cling to a wall
 		elif is_on_wall():
-			#print(position.x)
 			current_wall = int(position.x)
 			if recent_wall != current_wall:
-				#need to handle it so there can only be one wall jump on a single wall at a time
 				velocity.y = 0
-				#set current wall to the wall the player is currently on 
-				
-				print(current_wall)
 				attached_to_wall = true
 				
+		#if going to use a double jump
 		elif double_jump_count == 0:
 			velocity.y = JUMP_VELOCITY
 			double_jump_count += 1
+			
+		#(all other cases, no jump is performed)
 		
 	if Input.is_action_just_released("Accept") and velocity.y < -60:
 		velocity.y = - 60
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("Left", "Right")
+	#only walk when there is a direction input and the player is not clinging to a wall
 	if direction and not attached_to_wall:
 		if sprite.animation != "walk":
 			sprite.play("walk")  # Play the running animation
