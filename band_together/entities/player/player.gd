12d8a0d43c19e7e0599077728459fdc555c2e5c7
Helen Gezahegn <23146829+HelenGezahegn@users.noreply.github.com>
2025-02-09 12:25:33 -0700

    Revert "feat: dash implementation (#37)"
    This reverts commit f771927cd8722f54372e94929516d29809d29bf6.

--- band_together/entities/player/player.gd
+++ band_together/entities/player/player.gd
@@ -4,17 +4,7 @@ extends CharacterBody2D
 const SPEED: float = 100.0
 const JUMP_VELOCITY: float = -300.0
 var double_jump_count: int = 0
-
-var DASH_MULT = 1
-var GRAV_DIV = 1
 #region
-
-#region Dash bools
-var is_charging = false
-var is_dashing = false
-#region
-
-var direction = 0 
 
 #region Walljump info
 var attached_to_wall = false
@@ -29,9 +19,6 @@ func _physics_process(delta):
 	# Add the gravity, whenever the player is both not on the ground or clinging to a wall
 	if not is_on_floor() and not attached_to_wall:
 		velocity += get_gravity() * delta
-		#if the player is falling, and is either charging their dash or dashing, slow their descent
-		if velocity.y > 0 and (is_charging or is_dashing):
-			velocity.y /= GRAV_DIV
 	# Reset double and wall jumps when the player hits the ground
 	elif is_on_floor():
 		double_jump_count = 0
@@ -73,23 +60,14 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_released("Accept") and velocity.y < -60:
 		velocity.y = - 60
-		
-	# Handle Dash - Will eventually be associated with the saxophones movement ability
-	# Starts the charge up for the dash
-	if Input.is_action_just_pressed("Dash"): 
-		if !is_charging and !is_dashing: # prevents repeated dashing
-			#Start charging up a dash, reduces fall speed
-			GRAV_DIV = 2
-			is_charging = true
-			$DashChargeTimer.start()
-		
+
 	# Get the input direction and handle the movement/deceleration.
-	direction = Input.get_axis("Left", "Right")
+	var direction = Input.get_axis("Left", "Right")
 	#only walk when there is a direction input and the player is not clinging to a wall
 	if direction and not attached_to_wall:
 		if sprite.animation != "walk":
 			sprite.play("walk")  # Play the running animation
-		velocity.x = direction * SPEED * DASH_MULT
+		velocity.x = direction * SPEED
 		# Flip the sprite based on direction
 		if direction > 0:
 			sprite.flip_h = false  # Face right
@@ -103,19 +81,3 @@ func _physics_process(delta):
 			sprite.play("idle")
 		
 	move_and_slide()
-	
-	
-
-
-func _on_dash_charge_timer_timeout() -> void:
-	#start the dash, increase the speed of the player
-	DASH_MULT = 2
-	is_charging = false
-	is_dashing = true
-	$DashExecuteTimer.start()
-	
-func _on_dash_execute_timer_timeout() -> void:
-	#dash over, return to normal movement and fall speeds
-	DASH_MULT = 1
-	GRAV_DIV = 1
-	is_dashing = false
