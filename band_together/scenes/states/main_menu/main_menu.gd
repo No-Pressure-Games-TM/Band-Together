0a76f3bf8f1df6e737a2204c08b2ab9e8a79066c
Helen Gezahegn <23146829+HelenGezahegn@users.noreply.github.com>
2025-02-10 00:06:08 -0700

    feat: keyboard + controller handling for menu screens (#42)
    * feat: game over ui + button updates
    
    * fix: project.godot syntax
    
    * fix: main_menu.tscn syntax
    
    * fix: linked game over state to game over screen
    
    * fix: moved Pause Panel to UI Node instead of Hearts Node
    
    * fix: pause screen buttons + z-index overlay
    
    * fix: clear debugger errors
    
    * feat: add keyboard + controller handling for menu, pause, and game over screens

--- band_together/scenes/states/main_menu/main_menu.gd
+++ band_together/scenes/states/main_menu/main_menu.gd
@@ -1,15 +1,14 @@
 extends Control
 
+@onready var start_game_btn: Button = $VBoxContainer/StartGame
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	start_game_btn.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/test/test_walljump.tscn")
--- band_together/scenes/states/pause.gd
+++ band_together/scenes/states/pause.gd
@@ -1,11 +1,11 @@
 extends Node
 
 @onready var pause_panel: Panel = %PausePanel
+@onready var resume_btn: Button = $PausePanel/VBoxContainer/Resume
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:	
@@ -13,6 +13,7 @@ func _process(_delta: float) -> void:
 	var esc_pressed = Input.is_action_just_pressed("Pause")
 	
 	if (esc_pressed):
+		resume_btn.grab_focus()
 		get_tree().paused = true
 		pause_panel.show()
 	
