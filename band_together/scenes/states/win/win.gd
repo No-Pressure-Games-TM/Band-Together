cfa4e86f77f4f64f3667d09be935f5ffd26aec3e
Helen Gezahegn <helen.gezahegn@wealthsimple.com>
2025-02-17 15:12:52 -0700

    chore: delete extra space from win.gd

--- band_together/scenes/states/win/win.gd
+++ band_together/scenes/states/win/win.gd
@@ -13,6 +13,5 @@ func _process(delta: float) -> void:
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/states/main_menu/main_menu.tscn")
 
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
