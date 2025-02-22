4d2ec69f986d0463286671c1e03c44fc7a12f57e
jfjeld15 <jfjeld15@gmail.com>
2025-02-21 17:04:14 -0700

    Player code refactoring for organization

--- band_together/entities/player/camera.gd
+++ band_together/entities/player/camera.gd
@@ -15,6 +15,13 @@ func random_offset() -> Vector2:
 	return Vector2(rng.randf_range(-shake_strength, shake_strength),
 				   rng.randf_range(-shake_strength, shake_strength))
 
+func set_limits(bottom_limit, top_limit, right_limit, left_limit):
+	# Sets the bounds that the camera cannot go past
+	limit_bottom = bottom_limit
+	limit_right = right_limit
+	limit_left = left_limit
+	limit_top = top_limit
+
 func _process(delta):
 	if shake_strength > 0:
 		shake_strength = lerpf(shake_strength, 0, shake_fade * delta)
--- band_together/entities/player/player.gd
+++ band_together/entities/player/player.gd
@@ -42,65 +42,13 @@ var default_i_frame_timer: float = 1.3  # number of seconds to be invincible aft
 
 func _ready():
 	# Set the camera limits to those in the editor
-	camera.limit_bottom = bottom_limit
-	camera.limit_right = right_limit
-	camera.limit_left = left_limit
-	camera.limit_top = top_limit
+	camera.set_limits(bottom_limit, top_limit, right_limit, left_limit)
 
 func _physics_process(delta):
-	##When the player presses the drum button, it enables the drum's hitbox and sets a timer that keeps it active for 0.15 seconds	
-	if Input.is_action_just_pressed("Drum"):
-		$DrumArea/DrumAttack.disabled = false
-		$DrumArea/DrumAttack/DrumTimer.start()
-			
-	##When the player presses the baton button, it enables the drum's hitbox and sets a timer that keeps it active for 0.15 seconds		
-	if Input.is_action_just_pressed("Baton"):
-		$BatonArea/BatonAtack.disabled = false
-		$BatonArea/BatonAtack/BatonTimer.start()
-	
-	if moving_allowed:
-		direction = Input.get_axis("Left", "Right")
-	else:
-		direction = 0
-		
-	wall_attach()
-	gravity(delta)  # Wall slide, jump buffer, coyote time is also in here!
-	## I commented this out for now. Will add it back in when I am done organizing - jon
-	#if not is_on_floor() and not attached_to_wall:
-		#velocity += get_gravity() * delta
-		##if the player is falling, and is either charging their dash or dashing, slow their descent
-		#if velocity.y > 0 and (is_charging or is_dashing):
-			#velocity.y /= grav_div
-	jump(delta)
-	
-		
-	# Handle Dash - Will eventually be associated with the saxophones movement ability
-	# Starts the charge up for the dash
-	if Input.is_action_just_pressed("Dash"): 
-		if !is_charging and !is_dashing: # prevents repeated dashing
-			#Start charging up a dash, reduces fall speed
-			grav_div = 2
-			is_charging = true
-			$DashChargeTimer.start()
-			
-	#only walk when there is a direction input and the player is not clinging to a wall
-	if direction and not attached_to_wall:
-		if sprite.animation != "walk":
-			sprite.play("walk")  # Play the running animation
-		velocity.x = move_toward(velocity.x, direction * speed * dash_mult, speed) 
-		# Flip the sprite based on direction
-		if direction > 0:
-			sprite.flip_h = false  # Face right
-		elif direction < 0:
-			sprite.flip_h = true # Face left
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		
-		# play the idle animation if not already playing
-		if sprite.animation != "idle" and is_on_floor():
-			sprite.play("idle")
-	
-	move_and_slide()
+	check_input()  # Attacks, direction input, wall attaching
+	gravity(delta)  # Slow fall, wall slide, jump buffer, coyote time is also in here!
+	jump(delta)  # All types of jumps (wall jump, double jump, etc!)
+	move_and_animate()  # Sets velocity based on input, changes sprites
 
 func _process(delta):
 	if i_frame_timer > 0:
@@ -110,26 +58,36 @@ func _process(delta):
 	if UI.lives <= 0:
 		pause_movement(3)  # This makes it so the player cannot walk around if they die (before game over screen)
 
-#Re-disables the attack hitbox after the agreed upon duration
-func _on_drum_timer_timeout() -> void:
-	$DrumArea/DrumAttack.disabled = true
-
-#Re-disables the attack hitbox after the agreed upon duration
-func _on_baton_timer_timeout() -> void:
-	$BatonArea/BatonAtack.disabled = true 
-
-##Consequence for enemies hitting the attack hitbox
-#Currently, as there is no health system for enemies, this rotates them :D
-func _on_drum_area_body_entered(body: Node2D) -> void:
-	if body.name != "Player":
-		body.rotate(1)
-		
-
-##Consequence for enemies hitting the attack hitbox
-#Currently, as there is no health system for enemies, this rotates them :D
-func _on_baton_area_body_entered(body: Node2D) -> void:
-	if body.name != "Player":
-		body.rotate(1)
+func check_input() -> void:
+	## When the player presses the drum button, it enables the drum's hitbox and sets a timer that keeps it active for 0.15 seconds	
+	if Input.is_action_just_pressed("Drum"):
+		$DrumArea/DrumAttack.disabled = false
+		$DrumArea/DrumAttack/DrumTimer.start()
+			
+	## When the player presses the baton button, it enables the drum's hitbox and sets a timer that keeps it active for 0.15 seconds		
+	if Input.is_action_just_pressed("Baton"):
+		$BatonArea/BatonAtack.disabled = false
+		$BatonArea/BatonAtack/BatonTimer.start()
+	
+	# Handle Dash - Will eventually be associated with the saxophones movement ability
+	# Starts the charge up for the dash
+	if Input.is_action_just_pressed("Dash"): 
+		if !is_charging and !is_dashing: # prevents repeated dashing
+			#Start charging up a dash, reduces fall speed
+			grav_div = 2
+			is_charging = true
+			$DashChargeTimer.start()
+	
+	if moving_allowed:
+		direction = Input.get_axis("Left", "Right")
+	else:
+		direction = 0
+	
+	# Wall attaching
+	if is_on_wall_only() and direction == -get_wall_normal().x:
+		attached_to_wall = true
+	elif direction == get_wall_normal().x or Input.is_action_just_pressed("Down") or !is_on_wall():
+		attached_to_wall = false
 
 func gravity(delta) -> void:
 	if is_on_floor():
@@ -139,6 +97,11 @@ func gravity(delta) -> void:
 	elif attached_to_wall:
 		velocity.y = wall_slide_speed * delta  # Slow gravity when sliding on wall
 		coyote_time_wall_counter = coyote_time
+	elif not is_on_floor() and not attached_to_wall:
+		velocity += get_gravity() * delta
+		#if the player is falling, and is either charging their dash or dashing, slow their descent
+		if velocity.y > 0 and (is_charging or is_dashing):
+			velocity.y /= grav_div
 	elif velocity.y > 0:
 		coyote_time_counter -= delta
 		coyote_time_wall_counter -= delta
@@ -181,13 +144,34 @@ func jump(delta) -> void:
 	if Input.is_action_just_released("Accept") and velocity.y < -30:
 		velocity.y = - 30
 
-func wall_attach() -> void:
-	if is_on_wall_only() and direction == -get_wall_normal().x:
-		attached_to_wall = true
-	elif direction == get_wall_normal().x or Input.is_action_just_pressed("Down") or !is_on_wall():
-		attached_to_wall = false
+func move_and_animate() -> void:
+	# only walk when there is a direction input and the player is not clinging to a wall
+	if direction and not attached_to_wall:
+		play_animation("walk")
+		velocity.x = move_toward(velocity.x, direction * speed * dash_mult, speed) 
+		# Flip the sprite based on direction
+		if direction > 0:
+			sprite.flip_h = false  # Face right
+		elif direction < 0:
+			sprite.flip_h = true # Face left
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		# play the idle animation if on floor
+		if is_on_floor():
+			play_animation("idle")
+		else:
+			# place wall slide animation here
+			# place falling animation here
+			# etc etc
+			pass
+			
+	move_and_slide()
 
-func take_damage(direction=null):
+func play_animation(animation_name) -> void:
+	if sprite.animation != animation_name:
+		sprite.play(animation_name)
+
+func take_damage(direction=null) -> void:
 	# Direction should == -1 or 1 (direction to launch player when taking damage)
 	if i_frame_timer <= 0:
 		i_frame_timer = default_i_frame_timer
@@ -205,7 +189,7 @@ func take_damage(direction=null):
 			velocity.y = -100
 			pause_movement(0.2)
 
-func pause_movement(howlong):
+func pause_movement(howlong) -> void:
 	# Briefly disable movement control (thanks chatgpt <3)
 	moving_allowed = false
 	await get_tree().create_timer(howlong, false).timeout
@@ -227,3 +211,23 @@ func _on_dash_execute_timer_timeout() -> void:
 func _on_win_area_body_entered(_body: Node2D) -> void:
 	print("You Win!\n")
 	SceneTransition.change_scene("res://scenes/states/win/win.tscn")
+
+#Re-disables the attack hitbox after the agreed upon duration
+func _on_drum_timer_timeout() -> void:
+	$DrumArea/DrumAttack.disabled = true
+
+#Re-disables the attack hitbox after the agreed upon duration
+func _on_baton_timer_timeout() -> void:
+	$BatonArea/BatonAtack.disabled = true 
+
+##Consequence for enemies hitting the attack hitbox
+#Currently, as there is no health system for enemies, this rotates them :D
+func _on_drum_area_body_entered(body: Node2D) -> void:
+	if body.name != "Player":
+		body.rotate(1)
+
+##Consequence for enemies hitting the attack hitbox
+#Currently, as there is no health system for enemies, this rotates them :D
+func _on_baton_area_body_entered(body: Node2D) -> void:
+	if body.name != "Player":
+		body.rotate(1)
