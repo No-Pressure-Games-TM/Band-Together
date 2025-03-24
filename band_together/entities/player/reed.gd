cbd00d4067763aed9724d92f01baa491267ccbf1
gareba <gaven@ualberta.ca>
2025-03-23 22:44:48 -0600

    chore: cleaned up reed.gd

--- band_together/entities/player/reed.gd
+++ band_together/entities/player/reed.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 var direction: int = 1
-@export var speedx:= 300
-@export var speedy:= 350
+@export var speed_x:= 300
+@export var speed_y:= 350
+@export var proj_dmg:= 10
+@export var proj_grav:= 10
 var angle: float = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Sprite2D.flip_h = direction > 0
@@ -13,14 +17,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#pass
-	position.x += speedx * delta * direction * sin(angle)
-	position.y -= speedy * delta * cos(angle)
-	speedy -= 10
+	position.x += speed_x * delta * direction * sin(angle)
+	position.y -= speed_y * delta * cos(angle)
+	speed_y -= proj_grav
 
-
+# copied from jons code (hayyyy) 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("hi!")
 	if body.is_in_group("enemy") and body.has_method("take_damage"):
 		var hit_dir = sign(body.position.x - position.x)
-		body.take_damage(10, hit_dir)
+		body.take_damage(proj_dmg, hit_dir)
 	
