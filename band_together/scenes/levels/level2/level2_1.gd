5a8ca13061a00a13c15237535f6aa44a80076a2d
Helen Gezahegn <helen.gezahegn@wealthsimple.com>
2025-04-06 03:59:23 -0600

    fix: level2_2 for difficulty + length

--- band_together/scenes/levels/level2/level2_1.gd
+++ band_together/scenes/levels/level2/level2_1.gd
@@ -6,7 +6,6 @@ extends Node2D
 @onready var door: Area2D = $Door
 @onready var player: CharacterBody2D = $Player
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.dash_enabled = false
